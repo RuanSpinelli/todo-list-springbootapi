c15e8398b134a249ee1fd7f2842ae0e3c7d847e0
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2025-06-02 14:38:03 -0300

    Update NoteRepository.java
    Agora é possível puxar as anotações com base no nome de seus donos

--- todo-list/src/main/java/com/todo/repository/NoteRepository.java
+++ todo-list/src/main/java/com/todo/repository/NoteRepository.java
@@ -1,7 +1,13 @@
 package com.todo.repository;
 
 import com.todo.model.Note;
+import com.todo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends JpaRepository<Note, Integer> {
+
+    List<Note> findByOwner(Usuario usuario);
+    
 }
