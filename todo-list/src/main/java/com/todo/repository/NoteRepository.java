e97d8779219f6df67228ad5fde5dc4e5dfebf1a3
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2025-06-14 20:34:38 -0300

    NoteService + NoteRepository
    Início da implementação de lógica nas anotações e a utilização do JPA para fazer o intermédio entre o objeto e a entidade no banco de dados

--- todo-list/src/main/java/com/todo/repository/NoteRepository.java
+++ todo-list/src/main/java/com/todo/repository/NoteRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface NoteRepository extends JpaRepository<Note, Integer> {
+public interface NoteRepository extends JpaRepository<Note, Long> {
 
     List<Note> findByOwner(Usuario usuario);
     
---
+++ todo-list/src/main/java/com/todo/service/NoteService.java
@@ -0,0 +1,46 @@
+package com.todo.service;
+
+import com.todo.model.Note;
+import com.todo.model.Usuario;
+import com.todo.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    // Puxa o repositório para poder ter acesso e manipular os dados
+    @Autowired
+    private NoteRepository noteRepository;
+
+    // Listar por usuário
+    public List<Note> listarPorUsuario(Usuario usuario) {
+        return noteRepository.findByOwner(usuario);
+    }
+
+    // Listar todas as anotações
+    public List<Note> ListarTodas() {
+        
+        return noteRepository.findAll();
+    }
+
+    // Buscar por Id
+    public Note buscarPorId(Long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Anotação não encontrada"));
+    }
+
+    // Salvar as anotações
+    public Note salvar(Note note) {
+        return noteRepository.save(note);
+    }
+
+    // Deletar uma anotação
+    public void deletar(Long id){
+        noteRepository.deleteById(id);
+    }
+
+
+
+}
