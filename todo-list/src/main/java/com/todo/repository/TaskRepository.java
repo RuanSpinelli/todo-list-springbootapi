2df31832ede242475fec8d8e1d3504356f6d08e6
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2025-06-14 22:47:35 -0300

    Update TaskRepository.java

--- todo-list/src/main/java/com/todo/repository/TaskRepository.java
+++ todo-list/src/main/java/com/todo/repository/TaskRepository.java
@@ -1,9 +1,12 @@
 package com.todo.repository;
 
+import com.todo.model.Note;
 import com.todo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+import java.util.List;
 
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByNote(Note note);
 
 }
