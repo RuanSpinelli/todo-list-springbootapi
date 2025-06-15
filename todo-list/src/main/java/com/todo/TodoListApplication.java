d527195eb3509c1d593fa2eda8e8b41235463544
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2025-06-14 23:47:39 -0300

    Update AppConfig.java
    cria um usuário admin ao executar apenas para testar o sistema

--- todo-list/src/main/java/com/todo/TodoListApplication.java
+++ todo-list/src/main/java/com/todo/TodoListApplication.java
@@ -1,13 +1,42 @@
 package com.todo;
 
+import com.todo.model.Role;
+import com.todo.model.Usuario;
+import com.todo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class TodoListApplication {
+public class TodoListApplication implements CommandLineRunner {
+
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		if (userRepository.findByName("admin").isEmpty()) {
+			Usuario admin = new Usuario();
+			admin.setName("admin");
+			admin.setPassword(passwordEncoder.encode("admin123"));
+			admin.setRole(Role.ADMIN);
+			userRepository.save(admin);
+			System.out.println("Usuário admin criado!");
+		} else {
+			System.out.println("Usuário admin já existe.");
+		}
 	}
 
 }
